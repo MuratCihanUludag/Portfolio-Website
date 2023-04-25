f6b760c05149a43ab230ae51141692c212e5f7bc
MuratCihanUludag <muratcihanuludag@hotmail.com>
2023-04-25 21:51:00 +0300

    Complated Experience

--- 3d_portfolio/src/components/Experience.jsx
+++ 3d_portfolio/src/components/Experience.jsx
@@ -9,8 +9,25 @@ import { textVariant } from '../utils/motion'
 
 const ExperienceCard = ({experience})=>{
   return(
-    <VerticalTimelineElement contentStyle={{background:"#1d1836",color:"#fff"}}>
-    {experience.date}
+    <VerticalTimelineElement contentStyle={{background:"#1d1836",color:"#fff"}} contentArrowStyle={{borderRight:"7px solid #232631"}} date={experience.date} iconStyle={{background:experience.iconBg}}
+    icon={
+      <div className='flex justify-center items-center w-full h-full' >
+        <img src={experience.icon} alt={experience.company_name} className='w-[60%] h-[60%] object-contain'/>
+      </div>
+    } >
+    <div>
+      <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
+      <p className='text-secondary text-[16px] font-semibold'> {experience.company_name}</p>
+      <ul>
+        {experience.points.map((point,index)=>{
+          return(
+            <li key={`experienc-point-${index}`} className='text-white-100 text-[14px] pl-1 tracking-wide ' >
+            {point}
+          </li>
+          )
+        })}
+      </ul>
+    </div>
     </VerticalTimelineElement>
   )
 }
