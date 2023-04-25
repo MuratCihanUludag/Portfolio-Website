031abe1117b2b87b7e71680fd0495e5d82444aac
MuratCihanUludag <muratcihanuludag@hotmail.com>
2023-04-25 21:27:02 +0300

    Create ExperienceCard

--- 3d_portfolio/src/components/Experience.jsx
+++ 3d_portfolio/src/components/Experience.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
-import { VerticalTimeLine, VerticalTimeLineElement } from "react-vertical-timeline-component"
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 import {motion} from "framer-motion"
 import { styles } from '../styles'
 import { experiences } from '../constants'
 import { SectionWrapper } from '../hoc'
 import { textVariant } from '../utils/motion'
-import "react-vertical-timeline-component/style.min.css"
+
+
+const ExperienceCard = ({experience})=>{
+  return(
+    <VerticalTimelineElement contentStyle={{background:"#1d1836",color:"#fff"}}>
+    {experience.date}
+    </VerticalTimelineElement>
+  )
+}
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>What I have done so far</p>
+        <h2 className={styles.sectionHeadText}>Work Experince.</h2>
+      </motion.div>
+      <div className='mt-20 flex flex-col'>
+        <VerticalTimeline>
+          {experiences.map((experience,index)=>{
+            return(
+              <ExperienceCard experience={experience} />
+            )
+          })}
+        </VerticalTimeline>
+      </div>
+    </>
   )
 }
 
